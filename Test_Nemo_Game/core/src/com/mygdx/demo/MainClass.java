332b21cf9c57fff998d3cbf72e7d130ec10f4ecb
Tanakornohm <33046296+Tanakornohm@users.noreply.github.com>
2018-11-25 17:16:04 +0700

    ทำให้ object ไม่สามารถทะลุขอบได้

--- Test_Nemo_Game/core/src/com/mygdx/demo/MainClass.java
+++ Test_Nemo_Game/core/src/com/mygdx/demo/MainClass.java
@@ -2,6 +2,7 @@ package com.mygdx.demo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.*;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -16,12 +17,13 @@ public class MainClass extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
-	float x;
-	float y;
+	float x = 0;
+	float y = 400;
 	
 	//ประกาศตัวแปร font
 	//BitmapFont font;
 	
+	//ฟังก์ชั่นนี้จะทำตอนเริ่มโปรแกรม
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -34,6 +36,7 @@ public class MainClass extends ApplicationAdapter {
 	
 	}
 
+	//ฟังก์ชั่นคือ จะทำตลอดเวลาตั้งแต่เปิดโปรแกรม
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 1, 0, 1);
@@ -49,19 +52,37 @@ public class MainClass extends ApplicationAdapter {
 		
 		
 		//ทำให้รูปภาพขยับ
-		y -= 200 * Gdx.graphics.getDeltaTime();
-		x += 100 * Gdx.graphics.getDeltaTime();
+//		y -= 200 * Gdx.graphics.getDeltaTime();
+//		x += 100 * Gdx.graphics.getDeltaTime();
+//		
+		
+		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			x += 500 * Gdx.graphics.getDeltaTime();
+		}
+		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
+			x -= 500 * Gdx.graphics.getDeltaTime();
+		}
+		//อันนี้คือเวลากดปุม ตัวอย่างคือ space bar
+		//if (Gdx.input.isKeyPressed(Keys.SPACE)) {
+		//	}
+		
 		//อันนี้คือเวลากดคลิ๊กจะ + แกน y เพิ่มเข้าไป
-		if (Gdx.input.isTouched()) {
-            y += 500 * Gdx.graphics.getDeltaTime();
-        }
+//		if (Gdx.input.isTouched()) {
+//            y += 500 * Gdx.graphics.getDeltaTime();
+//        }
 		//อันนี้คือเวลา x มันหลุดขอบ ก็ให้มันมาเริ่มที่ 0 ใหม่
-		if (y < 0 || x > WIDTH) {
-            x = 0;
-            y = HEIGHT / 2;
-        }
+		if (x > 750) {
+			x = 750;
+		}
+		if (x < 0) {
+			x = 0;
+		}
+//		if (y < 0 || x > WIDTH || y >600) {
+//            x = 0;
+//            y = HEIGHT / 2;
+//        }
 	}
-	
+	//ฟังก์ชั่นนี้จะทำงานตอนปิดโปรแกรม
 	@Override
 	public void dispose () {
 		batch.dispose();
