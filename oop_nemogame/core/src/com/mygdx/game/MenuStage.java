d071f3f0d48bb04949d4144de8313ca4c1cd9d5d
WaiWarit <33046422+WaiWarit@users.noreply.github.com>
2018-12-12 18:45:20 +0700

    gameState
    gameStateManager

---
+++ oop_nemogame/core/src/com/mygdx/game/MenuStage.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import GameManager.GameState;
+import GameManager.GameStateManager;
+
+public class MenuStage extends GameState {
+
+	public MenuStage(GameStateManager gsm) {
+		super(gsm);
+		
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		System.out.println("MENU");
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ oop_nemogame/core/src/com/mygdx/game/PlayState.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import GameManager.GameState;
+import GameManager.GameStateManager;
+
+public class PlayState extends GameState {
+
+	public PlayState(GameStateManager gsm) {
+		super(gsm);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render() {
+		System.out.println("PLAY");
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
